7253119ca951bcd0a3add16925f33ea2764bc050
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-10 22:23:23 +0300

    feat: create place-card component and type PlaceCardProps

---
+++ project/src/components/place-card/place-card.type.ts
@@ -0,0 +1,10 @@
+type PlaceCardProps = {
+  isFavorite: boolean;
+  isPremium: boolean;
+  previewImage: string;
+  price: number;
+  title: string;
+  type: string;
+};
+
+export default PlaceCardProps;
