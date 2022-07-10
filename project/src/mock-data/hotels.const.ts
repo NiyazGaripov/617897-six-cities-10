7bc03613fc9b2375572488d5955e700c0504a138
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-10 22:12:25 +0300

    feat: create mock data

---
+++ project/src/mock-data/hotels.const.ts
@@ -0,0 +1,196 @@
+import Hotel from '../types/hotel.type';
+
+const HOTELS: Hotel[] = [
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating'
+    ],
+    host: {
+      avatarUrl: 'img/apartment-01.jpg',
+      id: 1,
+      isPro: true,
+      name: 'Angelina',
+    },
+    id: 1,
+    images: [
+      'img/apartment-01.jpg',
+    ],
+    isFavorite: false,
+    isPremium: true,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/apartment-01.jpg',
+    price: 120,
+    rating: 4.8,
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    bedrooms: 2,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating',
+    ],
+    host: {
+      avatarUrl: 'img/room.jpg',
+      id: 2,
+      isPro: false,
+      name: 'Keks',
+    },
+    id: 2,
+    images: [
+      'img/room.jpg'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 2,
+    previewImage: 'img/room.jpg',
+    price: 80,
+    rating: 4.9,
+    title: 'Wood and stone place',
+    type: 'Private room',
+  },
+  {
+    bedrooms: 4,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating',
+    ],
+    host: {
+      avatarUrl: 'img/apartment-02.jpg',
+      id: 2,
+      isPro: false,
+      name: 'Alice',
+    },
+    id: 3,
+    images: [
+      'img/apartment-02.jpg'
+    ],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/apartment-02.jpg',
+    price: 132,
+    rating: 4.8,
+    title: 'Canal View Prinsengracht',
+    type: 'Apartment',
+  },
+  {
+    bedrooms: 5,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating',
+    ],
+    host: {
+      avatarUrl: 'img/apartment-03.jpg',
+      id: 2,
+      isPro: false,
+      name: 'Alice',
+    },
+    id: 4,
+    images: [
+      'img/apartment-03.jpg'
+    ],
+    isFavorite: false,
+    isPremium: true,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 5,
+    previewImage: 'img/apartment-03.jpg',
+    price: 180,
+    rating: 4.8,
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+  },
+  {
+    bedrooms: 2,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating',
+    ],
+    host: {
+      avatarUrl: 'img/room.jpg',
+      id: 2,
+      isPro: false,
+      name: 'Keks',
+    },
+    id: 5,
+    images: [
+      'img/room.jpg'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 2,
+    previewImage: 'img/room.jpg',
+    price: 80,
+    rating: 4.9,
+    title: 'Wood and stone place',
+    type: 'Private room',
+  },
+];
+
+export default HOTELS;
