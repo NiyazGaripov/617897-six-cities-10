132ab7b4c6892ad98a5822ddb8b775dfcab6d269
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-11 13:48:53 +0300

    feat: add type to const CITIES

--- project/src/mock-data/cities.const.ts
+++ project/src/mock-data/cities.const.ts
@@ -1,3 +1,3 @@
-const CITIES = ['Paris','Cologne','Brussels','Amsterdam','Hamburg','Dusseldorf'];
+const CITIES: string[] = ['Paris','Cologne','Brussels','Amsterdam','Hamburg','Dusseldorf'];
 
 export default CITIES;
