3751ff5151319524b03854e9b7d89887c1c347dc
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-11 13:52:26 +0300

    feat: create const RATINGS

---
+++ project/src/mock-data/raitings.const.ts
@@ -0,0 +1,31 @@
+import Rating from '../types/raiting.type';
+
+const RATINGS: Rating[] = [
+  {
+    value: '1',
+    id: '1-star',
+    title: 'terribly',
+  },
+  {
+    value: '2',
+    id: '2-star',
+    title: 'badly',
+  },
+  {
+    value: '3',
+    id: '3-star',
+    title: 'not bad',
+  },
+  {
+    value: '4',
+    id: '4-star',
+    title: 'good',
+  },
+  {
+    value: '5',
+    id: '5-star',
+    title: 'perfect',
+  },
+];
+
+export default RATINGS;
