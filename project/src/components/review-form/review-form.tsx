fa4a14d3b81ff85f2c54578b0f4b727ac4f0dbfb
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-11 14:15:38 +0300

    feat: create review-form component and type ReviewFormProps

---
+++ project/src/components/review-form/review-form.tsx
@@ -0,0 +1,37 @@
+import ReviewFormProps from './review-form.type';
+import Rating from '../../types/raiting.type';
+
+function ReviewForm({ratings}: ReviewFormProps): JSX.Element {
+  const inputs = ratings.map((rating: Rating): JSX.Element =>
+    (
+      <>
+        <input className="form__rating-input visually-hidden" name="rating" value={rating.value} id={rating.id} type="radio" />
+        <label htmlFor={rating.id} className="reviews__rating-label form__rating-label" title={rating.title}>
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+      </>
+    )
+  );
+
+  return (
+    <form className="reviews__form form" action="#" method="post">
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        {inputs}
+      </div>
+
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and
+          describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+      </div>
+    </form>
+  );
+}
+
+export default ReviewForm;
---
+++ project/src/components/review-form/review-form.type.ts
@@ -0,0 +1,7 @@
+import Rating from '../../types/raiting.type';
+
+type ReviewFormProps = {
+  ratings: Rating[];
+};
+
+export default ReviewFormProps;
