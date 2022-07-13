48c4289f4d60b27a883f84b710cabb763787719f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-13 10:36:09 +0300

    feat: create mock data for comments

---
+++ project/src/mock-data/comments.const.ts
@@ -0,0 +1,18 @@
+import Comment from '../types/comment.type';
+
+const COMMENTS: Comment[] = [
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+    date: new Date().toISOString(),
+    id: 1,
+    rating: 4.7,
+    author: {
+      avatarUrl: 'img/avatar-max.jpg',
+      id: 15,
+      isPro: true,
+      name: 'Max',
+    },
+  },
+];
+
+export default COMMENTS;
