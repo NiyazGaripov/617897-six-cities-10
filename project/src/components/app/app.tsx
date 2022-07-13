42552ada7820208e7ac594e48632e398f08ecfa1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-13 10:33:11 +0300

    feat: add routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,14 +1,56 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import AppProps from './app.type';
+import {AppRoute} from '../../constants';
+import FAVORITE_LOCATIONS from '../../mock-data/favorite-locations.const';
+import HOTELS from '../../mock-data/hotels.const';
+import COMMENTS from '../../mock-data/comments.const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
 
 function App(props: AppProps): JSX.Element {
   return (
-    <Main
-      hotels={props.hotels}
-      cities={props.cities}
-      placesCount={props.placesCount}
-      user={props.user}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <Main
+              hotels={props.hotels}
+              cities={props.cities}
+              placesCount={props.placesCount}
+              user={props.user}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <Favorites
+              user={props.user}
+              locations={FAVORITE_LOCATIONS}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={
+            <Property
+              isAuth
+              user={props.user}
+              hotel={HOTELS[0]}
+              comments={COMMENTS}
+              nearbyHotels={props.hotels}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
