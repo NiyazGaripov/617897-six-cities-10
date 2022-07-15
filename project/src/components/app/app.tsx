872ff7b8a50d9dc4dc34a233c6211fd5b9ba7b00
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-15 10:09:38 +0300

    feat: add PrivateRoute for Favorites

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,15 +1,16 @@
 import {BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import AppProps from './app.type';
-import {AppRoute} from '../../constants';
+import {AppRoute, AuthorizationStatus} from '../../constants';
 import FAVORITE_LOCATIONS from '../../mock-data/favorite-locations.const';
 import HOTELS from '../../mock-data/hotels.const';
 import COMMENTS from '../../mock-data/comments.const';
+import CITIES from '../../mock-data/cities.const';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
 import Property from '../../pages/property/property';
 import {NotFound} from '../../pages/not-found/not-found';
-import CITIES from '../../mock-data/cities.const';
+import {PrivateRoute} from '../private-route/private-route';
 
 function App(props: AppProps): JSX.Element {
   return (
@@ -37,10 +38,12 @@ function App(props: AppProps): JSX.Element {
         <Route
           path={AppRoute.Favorites}
           element={
-            <Favorites
-              user={props.user}
-              locations={FAVORITE_LOCATIONS}
-            />
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites
+                user={props.user}
+                locations={FAVORITE_LOCATIONS}
+              />
+            </PrivateRoute>
           }
         />
         <Route
