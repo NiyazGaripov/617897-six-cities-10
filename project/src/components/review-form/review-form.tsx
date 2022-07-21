3c3fc89360928eeb447c8bf180d85dcfe1b6925a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-21 23:11:51 +0300

    feat: implement saving data entered into form in state of component

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,10 +1,25 @@
+import {ChangeEvent, useState} from 'react';
 import {Rating} from '../../types/raiting.type';
 
 type Props = {
   ratings: Rating[];
 };
 
+enum ReviewLength {
+  MIN = 50,
+  MAX = 300
+}
+
 export function ReviewForm({ratings}: Props): JSX.Element {
+  const [formData, setFormData] = useState({
+    rating: '0',
+    review: '',
+  });
+
+  const formFieldChangeHandler = (name: string, value: number | string) => {
+    setFormData({...formData, [name]: value});
+  };
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
@@ -13,7 +28,16 @@ export function ReviewForm({ratings}: Props): JSX.Element {
           ratings.map((rating) =>
             (
               <div key={rating.id}>
-                <input className="form__rating-input visually-hidden" name="rating" value={rating.value} id={rating.id} type="radio" />
+                <input
+                  className="form__rating-input visually-hidden"
+                  name="rating"
+                  value={rating.value}
+                  id={rating.id}
+                  type="radio"
+                  onClick={({currentTarget}) => {
+                    formFieldChangeHandler(currentTarget.name, rating.value);
+                  }}
+                />
                 <label htmlFor={rating.id} className="reviews__rating-label form__rating-label" title={rating.title}>
                   <svg className="form__star-image" width="37" height="33">
                     <use xlinkHref="#icon-star" />
@@ -25,10 +49,29 @@ export function ReviewForm({ratings}: Props): JSX.Element {
         }
       </div>
 
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" />
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={formData.review}
+        onChange={(evt: ChangeEvent<HTMLTextAreaElement>) => {
+          const {name, value} = evt.target;
+          formFieldChangeHandler(name, value);
+        }}
+      />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.</p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button
+          className="reviews__submit form__submit button"
+          type="submit"
+          disabled={
+            formData.rating === '0' &&
+            (formData.review.length < ReviewLength.MIN || formData.review.length > ReviewLength.MAX)
+          }
+        >
+          Submit
+        </button>
       </div>
     </form>
   );
