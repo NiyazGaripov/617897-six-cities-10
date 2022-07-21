541a5b5f8da2da891d54bc2f7bc0d4281f86b557
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-07-21 23:09:24 +0300

    refactor: correct const raitings

--- project/src/mocks/raitings.const.ts
+++ project/src/mocks/raitings.const.ts
@@ -2,19 +2,9 @@ import {Rating} from '../types/raiting.type';
 
 export const RATINGS: Rating[] = [
   {
-    value: '1',
-    id: '1-star',
-    title: 'terribly',
-  },
-  {
-    value: '2',
-    id: '2-star',
-    title: 'badly',
-  },
-  {
-    value: '3',
-    id: '3-star',
-    title: 'not bad',
+    value: '5',
+    id: '5-star',
+    title: 'perfect',
   },
   {
     value: '4',
@@ -22,8 +12,18 @@ export const RATINGS: Rating[] = [
     title: 'good',
   },
   {
-    value: '5',
-    id: '5-star',
-    title: 'perfect',
+    value: '3',
+    id: '3-star',
+    title: 'not bad',
+  },
+  {
+    value: '2',
+    id: '2-star',
+    title: 'badly',
+  },
+  {
+    value: '1',
+    id: '1-star',
+    title: 'terribly',
   },
 ];
