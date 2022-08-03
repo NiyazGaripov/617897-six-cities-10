15ec2c094d74b767b5cdb60b3ac82ab4a7e2e3f6
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-03 10:47:12 +0300

    feat: create component Map

---
+++ project/src/components/map/map.tsx
@@ -0,0 +1,57 @@
+import {useEffect, useRef} from 'react';
+import {Icon, Marker} from 'leaflet';
+import {City, Hotel} from '../../types/hotel.type';
+import {useMap} from '../../hooks/useMap';
+import {MAP_ICON} from '../../constants';
+import 'leaflet/dist/leaflet.css';
+
+
+type Props = {
+  city: City;
+  hotels: Hotel[];
+  activeHotelId: number | null;
+};
+
+const defaultCustomIcon = new Icon({
+  iconUrl: MAP_ICON.DEFAULT,
+  iconSize: [MAP_ICON.WIDTH, MAP_ICON.HEIGHT],
+  iconAnchor: [MAP_ICON.ANCHOR, MAP_ICON.HEIGHT],
+});
+
+const activeCustomIcon = new Icon({
+  iconUrl: MAP_ICON.ACTIVE,
+  iconSize: [MAP_ICON.WIDTH, MAP_ICON.HEIGHT],
+  iconAnchor: [MAP_ICON.ANCHOR, MAP_ICON.HEIGHT],
+});
+
+export function Map({city, hotels, activeHotelId}: Props): JSX.Element {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      hotels.forEach((hotel) => {
+        const marker = new Marker({
+          lat: hotel.location.latitude,
+          lng: hotel.location.longitude
+        });
+
+        marker
+          .setIcon(
+            activeHotelId !== undefined && hotel.id === activeHotelId
+              ? activeCustomIcon
+              : defaultCustomIcon
+          )
+          .addTo(map);
+      });
+    }
+  }, [map, hotels, activeHotelId]);
+
+  return (
+    <section
+      className="cities__map map"
+      style={{height: '1000px'}}
+      ref={mapRef}
+    />
+  );
+}
