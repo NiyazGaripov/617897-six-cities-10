70781c88c77020836147d0fdeaf3cc0c9ee1998f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-11 23:13:52 +0300

    feat: create types State and AppDispatch

--- project/src/mocks/favorite-locations.const.ts
+++
@@ -1,9 +0,0 @@
-import {HOTELS} from './hotels.const';
-import {FavoritePlace} from '../types/hotel.type';
-
-export const FAVORITE_LOCATIONS: FavoritePlace[] = [
-  {
-    city: 'Amsterdam',
-    hotels: HOTELS.filter((hotel) => hotel.isFavorite),
-  }
-];
