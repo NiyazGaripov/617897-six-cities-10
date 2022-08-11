e488eddc05477d7b054b2d1887a05401c5574caa
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-11 23:40:57 +0300

    refactor: replace type for prop city

--- project/src/components/favorite-location/favorite-location.tsx
+++ project/src/components/favorite-location/favorite-location.tsx
@@ -1,9 +1,9 @@
 import {Link} from 'react-router-dom';
-import {Hotel} from '../../types/hotel.type';
+import {City, Hotel} from '../../types/hotel.type';
 import {PlaceCard} from '../place-card/place-card';
 
 type Props = {
-  city: string;
+  city: City;
   hotels: Hotel[];
 };
 
@@ -12,8 +12,8 @@ export function FavoriteLocation({city, hotels}: Props): JSX.Element {
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
-          <Link to={`/${city.toLowerCase()}`} className="locations__item-link">
-            <span>{city}</span>
+          <Link to={`/${city.name.toLowerCase()}`} className="locations__item-link">
+            <span>{city.name}</span>
           </Link>
         </div>
       </div>
