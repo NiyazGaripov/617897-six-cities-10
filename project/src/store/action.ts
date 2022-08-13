9d25496b97aa263c8585dd0505b4099f06c46c22
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-13 17:51:05 +0300

    feat: create action setSortingType

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,5 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Hotel} from '../types/hotel.type';
+import {SortingOption} from '../types/sorting.type';
 
 export const setCity = createAction<City>('main/setCity');
 export const setRentalPlaces = createAction<Hotel[]>('main/setRentalPlaces');
+export const setSortingType = createAction<SortingOption>('main/setSortingType');
