95d26580f44b3e265cd0db9c6df43f57643d9075
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-13 18:00:34 +0300

    feat: create function sortPlaces

--- project/src/utils/common.ts
+++ project/src/utils/common.ts
@@ -1,4 +1,21 @@
+import {Hotel} from '../types/hotel.type';
+import {SortingOption} from '../types/sorting.type';
+import {SortingType} from '../constants';
+
 const MAX_PERCENTAGE = 100;
 const MAX_RATING = 5;
 
 export const transformRatingToPercentage = (rating: number): string => `${(MAX_PERCENTAGE / MAX_RATING) * rating}%`;
+
+export const sortPlaces = (places: Hotel[], sortingType: SortingOption) => {
+  switch (sortingType) {
+    case SortingType.LOW_TO_HIGH:
+      return places.sort((a, b) => a.price - b.price);
+    case SortingType.HIGH_TO_LOW:
+      return places.sort((a, b) => b.price - a.price);
+    case SortingType.TOP_RATED:
+      return places.sort((a, b) => b.rating - a.rating);
+    default:
+      return places;
+  }
+};
