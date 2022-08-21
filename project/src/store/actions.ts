19781a55861f354bb1a4089769b63fb9711592e0
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-21 21:34:22 +0300

    feat: create action redirectToRoute

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,6 +1,6 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Hotel} from '../types/hotel.type';
-import {AuthorizationStatus, DataLoadingStatus, SortingType} from '../constants';
+import {AppRoute, AuthorizationStatus, DataLoadingStatus, SortingType} from '../constants';
 
 export const setCity = createAction<City>('main/setCity');
 export const setRentalPlaces = createAction<Hotel[]>('main/setRentalPlaces');
@@ -9,3 +9,4 @@ export const requireAuthorization = createAction<AuthorizationStatus>('data/requ
 export const loadPlaces = createAction<Hotel[]>('data/loadPlaces');
 export const loadFavoritePlaces = createAction<Hotel[]>('data/loadFavoritePlaces');
 export const setDataLoadingStatus = createAction<DataLoadingStatus>('data/setDataLoadingStatus');
+export const redirectToRoute = createAction<AppRoute>('main/redirectToRoute');
