37bbe306dfc106dee92812c67a939aff453f1a95
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-21 22:09:44 +0300

    feat: create action setUserData

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,6 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Hotel} from '../types/hotel.type';
 import {AppRoute, AuthorizationStatus, DataLoadingStatus, SortingType} from '../constants';
+import {User} from '../types/user.type';
 
 export const setCity = createAction<City>('main/setCity');
 export const setRentalPlaces = createAction<Hotel[]>('main/setRentalPlaces');
@@ -10,3 +11,4 @@ export const loadPlaces = createAction<Hotel[]>('data/loadPlaces');
 export const loadFavoritePlaces = createAction<Hotel[]>('data/loadFavoritePlaces');
 export const setDataLoadingStatus = createAction<DataLoadingStatus>('data/setDataLoadingStatus');
 export const redirectToRoute = createAction<AppRoute>('main/redirectToRoute');
+export const setUserData = createAction<User>('login/setUserData');
