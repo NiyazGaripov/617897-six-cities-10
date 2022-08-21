e3e221b02f56689002ba4d1e1b2eb6e7caae9809
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-21 17:33:26 +0300

    feat: implement work with auth form

--- project/src/components/login-form/login-form.tsx
+++ project/src/components/login-form/login-form.tsx
@@ -1,15 +1,58 @@
+import {FormEvent} from 'react';
+import {useAppDispatch} from '../../hooks';
+import {loginAction} from '../../store/api-actions';
+import {useFormField} from '../../hooks/useFormField';
+
 export function LoginForm(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const email = useFormField('',{ isEmpty: true, isEmail: true });
+  const password = useFormField('',{ isEmpty: true, minLength: 2, isPassword: true });
+
+  const handleFormSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+    dispatch(loginAction({ email: email.value, password: password.value }));
+  };
+
   return (
-    <form className="login__form form" action="#" method="post">
+    <form className="login__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <div className="login__input-wrapper form__input-wrapper">
         <label className="visually-hidden">E-mail</label>
-        <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+        <input
+          className="login__input form__input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={email.value}
+          onChange={email.onChange}
+          onBlur={email.onBlur}
+        />
+        { (email.isDirty && email?.valid?.isEmpty) && <p style={{color: 'red'}}>The field cannot be empty</p> }
+        { (email.isDirty && email?.valid?.emailError) && <p style={{color: 'red'}}>Enter a valid email address</p> }
       </div>
       <div className="login__input-wrapper form__input-wrapper">
         <label className="visually-hidden">Password</label>
-        <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+        <input
+          className="login__input form__input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={password.value}
+          onChange={password.onChange}
+          onBlur={password.onBlur}
+        />
+        { (password.isDirty && password?.valid?.isEmpty) && <p style={{color: 'red'}}>The field cannot be empty</p> }
+        { (password.isDirty && password?.valid?.passwordError) && <p style={{color: 'red'}}>Password must consist of at least one letter and one number</p> }
+        { (password.isDirty && password?.valid?.minLengthError) && <p style={{color: 'red'}}>The password must be at least 2 characters long</p> }
       </div>
-      <button className="login__submit form__submit button" type="submit">Sign in</button>
+      <button
+        className="login__submit form__submit button"
+        type="submit"
+        disabled={!email?.valid?.inputValid || !password?.valid?.inputValid}
+      >
+        Sign in
+      </button>
     </form>
   );
 }
