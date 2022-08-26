340e9d1a61d9e21c762669a9fb0645718a3a7003
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-26 17:45:14 +0300

    refactor: transfer const RATINGS from file

--- project/src/constants.ts
+++ project/src/constants.ts
@@ -1,5 +1,3 @@
-import {Rating} from './types/raiting.type';
-
 export enum AppRoute {
   Main = '/',
   Login = '/login',
@@ -47,31 +45,3 @@ export enum DataLoadingStatus {
   Fulfilled = 'fulfilled',
   Rejected = 'rejected',
 }
-
-export const RATINGS: Rating[] = [
-  {
-    value: '5',
-    id: '5-star',
-    title: 'perfect',
-  },
-  {
-    value: '4',
-    id: '4-star',
-    title: 'good',
-  },
-  {
-    value: '3',
-    id: '3-star',
-    title: 'not bad',
-  },
-  {
-    value: '2',
-    id: '2-star',
-    title: 'badly',
-  },
-  {
-    value: '1',
-    id: '1-star',
-    title: 'terribly',
-  },
-];
