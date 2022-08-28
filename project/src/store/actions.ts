aea66d9ee7060a733ce91c7dc60aaaad8da632d8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-28 16:55:14 +0300

    refactor: remove unused actions

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,19 +1,4 @@
 import {createAction} from '@reduxjs/toolkit';
-import {City, Hotel} from '../types/hotel.type';
-import {AppRoute, AuthorizationStatus, DataLoadingStatus, SortingType} from '../constants';
-import {User} from '../types/user.type';
-import {Comment} from '../types/comment.type';
+import {AppRoute} from '../constants';
 
-export const setCity = createAction<City>('main/setCity');
-export const setRentalPlaces = createAction<Hotel[]>('main/setRentalPlaces');
-export const setSortingType = createAction<SortingType>('main/setSortingType');
-export const requireAuthorization = createAction<AuthorizationStatus>('data/requireAuthorization');
-export const loadPlaces = createAction<Hotel[]>('data/loadPlaces');
-export const loadPlace = createAction<Hotel>('data/loadPlace');
-export const loadFavoritePlaces = createAction<Hotel[]>('data/loadFavoritePlaces');
-export const loadNearbyPlaces = createAction<Hotel[]>('data/loadNearbyPlaces');
-export const loadComments = createAction<Comment[]>('data/loadComments');
-export const addNewComment = createAction<Comment[]>('data/addNewComment');
-export const setDataLoadingStatus = createAction<DataLoadingStatus>('data/setDataLoadingStatus');
 export const redirectToRoute = createAction<AppRoute>('main/redirectToRoute');
-export const setUserData = createAction<User>('login/setUserData');
