d3bb6c434e76a59e589d785454333fc2bc29bd97
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-29 21:18:55 +0300

    refactor: replace type undefined to null

--- project/src/store/auth/selectors.ts
+++ project/src/store/auth/selectors.ts
@@ -3,5 +3,5 @@ import {AuthorizationStatus, NameSpace} from '../../constants';
 import {User} from '../../types/user.type';
 
 export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.Auth].authorizationStatus;
-export const getUser = (state: State): User | undefined => state[NameSpace.Auth].user;
+export const getUser = (state: State): User | null => state[NameSpace.Auth].user;
 export const getLoadingStatus = (state: State): boolean => state[NameSpace.Auth].loading;
