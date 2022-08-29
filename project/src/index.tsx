5497ef98564c8b3aca2a103eed73bdb971d80596
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-29 21:13:00 +0300

    refactor: remove React.StrictMode due to sending requests multiple times

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {ToastContainer} from 'react-toastify';
 import {store} from './store';
-import {checkAuthAction, fetchPlacesAction} from './store/api-actions';
 import {App} from './components/app/app';
 import 'react-toastify/dist/ReactToastify.css';
+import {fetchPlacesAction} from './store/places/api';
+import {checkAuthAction} from './store/auth/api';
 
 store.dispatch(fetchPlacesAction());
 store.dispatch(checkAuthAction());
@@ -15,10 +16,8 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ToastContainer />
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ToastContainer />
+    <App />
+  </Provider>
 );
