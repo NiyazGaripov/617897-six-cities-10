28a1147120f25adca46f74165637e87f982f3579
Niyaz Garipov <Niayz_Indicus@mail.ru>
2022-08-29 21:15:55 +0300

    feat: create selector getFavoritePlacesByCity

--- project/src/store/places/selectors.ts
+++ project/src/store/places/selectors.ts
@@ -5,6 +5,10 @@ import {NameSpace} from '../../constants';
 import {getActiveSortingType, getSelectCity} from '../app/selectors';
 import {filterPlaces, sortPlaces} from '../../utils/common';
 
+type FavoritePlacesByCity = {
+  [key: string]: Hotel[];
+};
+
 export const getPlaces = (state: State): Hotel[] => state[NameSpace.Places].places;
 export const getPlace = (state: State): Hotel | undefined => state[NameSpace.Places].place;
 export const getFavoritePlaces = (state: State): Hotel[] => state[NameSpace.Places].favoritePlaces;
@@ -19,3 +23,18 @@ export const getSortedPlaces = createSelector(
   getFilteredPlacesByCity,
   getActiveSortingType,
   (places, sortingType) => sortPlaces(places, sortingType));
+
+export const getFavoritePlacesByCity = createSelector(
+  getFavoritePlaces,
+  (favoritePlaces) => favoritePlaces.reduce<FavoritePlacesByCity>((acc, place) => {
+    const city = place.city;
+
+    if (!acc[city.name]) {
+      acc[city.name] = [];
+    }
+
+    acc[city.name].push(place);
+
+    return acc;
+  }, {})
+);
